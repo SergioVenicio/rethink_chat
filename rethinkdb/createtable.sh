3ad4f9c526b8eeac796b92aa82308df15242dca8
sergio <sergiovenicio2015@gmail.com>
2024-01-04 22:21:01 -0300

    chore: remove unnecessary file

--- rethinkdb/createtable.sh
+++
@@ -1,8 +0,0 @@
-r.dbList().contains('todos')
-  .do(function(databaseExists) {
-    return r.branch(
-      databaseExists,
-      { dbs_created: 0 },
-      r.dbCreate('todos')
-    );
-  }).run();
